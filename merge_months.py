9fa3f1c0727b4136176c9ce12efe9aecf65521fd
ntua-el18119 <94013739+ntua-el18119@users.noreply.github.com>
2023-02-05 21:14:30 +0200

    Update merge_months.py

--- merge_months.py
+++ merge_months.py
@@ -1,1 +1,19 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder.getOrCreate()
+df1 = spark.read.parquet("hdfs://master:9000/myfolder/january.parquet")
+
+df2 = spark.read.parquet("hdfs://master:9000/myfolder/february.parquet")
+
+df3 = spark.read.parquet("hdfs://master:9000/myfolder/march.parquet")
+
+df4 = spark.read.parquet("hdfs://master:9000/myfolder/april.parquet")
+
+df5 = spark.read.parquet("hdfs://master:9000/myfolder/may.parquet")
+
+df6 = spark.read.parquet("hdfs://master:9000/myfolder/june.parquet")
+mergedDF = df1.union(df2).union(df3).union(df4).union(df5).union(df6)
+#mergedDF.write.mode("overwrite").parquet("file:///home/user/all_months.parquet")
+mergedDF.write.mode("overwrite").parquet("/myfolder/all_final.parquet")
