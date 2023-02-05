b01c10aeba8a4aa494dbfb9b8390bc719d497b9b
ntua-el18119 <94013739+ntua-el18119@users.noreply.github.com>
2023-02-05 21:10:32 +0200

    Update q4.py

--- questions/q4.py
+++ questions/q4.py
@@ -1,1 +1,23 @@
+# Start a Spark session
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+from pyspark.sql import SparkSession
+# Convert "tpep_pickup_datetime" column into a date-time format and extract hour and day of the week
+spark = SparkSession.builder.appName("TaxiTrips").getOrCreate()
+df = spark.read.parquet("hdfs://master:9000/myfolder/taxi_trips/all_months_data.parquet")
+df = df.withColumn("pickup_datetime", F.to_timestamp(df["tpep_pickup_datetime"], "yyyy-MM-dd HH:mm:ss"))
+df = df.withColumn("hour", F.hour(df["pickup_datetime"]))
+df = df.withColumn("day_of_week", F.date_format(df["pickup_datetime"], "E"))
 
+# Group by hour and day of the week and count the number of passengers
+grouped_df = df.groupBy("hour", "day_of_week").agg(F.count("Passenger_count").alias("passenger_count"))
+grouped_df.show(1000)
+# Use window function to calculate running total of passengers for each hour and day
+window_spec = Window.partitionBy("day_of_week").orderBy(F.desc("passenger_count"))
+grouped_df = grouped_df.withColumn("running_total", F.sum("passenger_count").over(window_spec))
+
+# Filter to only include the top three hours for each day of the week
+grouped_df = grouped_df.withColumn("row_num", F.row_number().over(window_spec))
+grouped_df = grouped_df.filter(grouped_df["row_num"] <= 3)
+# Display the result
+grouped_df.show(21)
